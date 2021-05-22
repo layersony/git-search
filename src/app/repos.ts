904b7b3241444268376a9a9d3fffe4a9c60fa66f
layersony <sammaingi5@gmail.com>
2021-05-23 00:36:24 +0300

    add more dat properities

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -3,5 +3,10 @@ export class Repos {
     public name:string,
     public html_url:string,
     public updated_at:Date,
+    public created_at: Date,
+    public forks: number,
+    public language:string,
+    public clone_url: string,
+    public homepage: string,
   ){}  
 }
--- src/app/userdetail.ts
+++ src/app/userdetail.ts
@@ -3,7 +3,18 @@ export class Userdetail {
     public login:string,
     public avatar_url:string,
     public repos_url:string,
-    public name:string
+    public name:string,
+    public url:string,
+    public blog:string,
+    public location:string,
+    public email:string,
+    public bio:string,
+    public twitter_username:string,
+    public public_repos:number,
+    public followers:string,
+    public following:string,
+    public created_at: Date,
+    public update_at: Date
   ){}
 }
 
