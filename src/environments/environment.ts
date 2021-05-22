fc4eb243cf24503515135e906500dfbeba61b1d4
layersony <sammaingi5@gmail.com>
2021-05-22 08:35:17 +0300

    apiurl add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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',
+  apiUrl: 'https://api.github.com/users/',
 };
 
 /*
