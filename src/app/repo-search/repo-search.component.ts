f5267b84b940a7653d92566ea3c28855c40dd55d
layersony <sammaingi5@gmail.com>
2021-05-24 01:31:55 +0300

    repo search add

---
+++ src/app/repo-search/repo-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-search',
+  templateUrl: './repo-search.component.html',
+  styleUrls: ['./repo-search.component.css']
+})
+export class RepoSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
