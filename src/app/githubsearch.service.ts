c17650baff64651340f091116d41bbf111a2c01c
layersony <sammaingi5@gmail.com>
2021-05-24 12:00:30 +0300

    api check

--- src/app/githubsearch.service.ts
+++ src/app/githubsearch.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
-// import { environment } from '../environments/environment.prod'
+// import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod'
 import { Userdetail } from './userdetail';
 import { Repos } from './repos';
 import { Searchrepo } from './searchrepo';
