af38de146da5497e8a700204a77997b12317f29e
layersony <sammaingi5@gmail.com>
2021-05-24 11:58:29 +0300

    shorten date content create

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ShortenPipe implements PipeTransform {
 
-  transform(value: string, maxWords: number) {
-    return null;
+  transform(value: any): any {
+    let uploadtime = new Date(value).getTime() // time on list
+    let tdate = new Date().getTime(); // current time
+    let diff = tdate - uploadtime; 
+    var diffday = Math.floor(diff% 86400000)% 7;
+    var diffHrs = Math.floor((diff % 86400000) / 3600000); // hours
+    var diffMins = Math.round(((diff % 86400000) % 3600000) / 60000);  // mins
+    if (diffHrs == 0){
+      return diffMins +' mins ago'
+    }
+    else if(diffday){
+      return diffday +' Days ago'
+    }
+    else if (diffHrs ){
+      return diffHrs + ' hr(s) ago'
+    }
   }
-
 }
